c20aeb3ad6a69fc60beca8162c167ad6b8dea3de
pipysk1 <49283427+pipysk1@users.noreply.github.com>
2025-02-05 12:51:06 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FE-ThAudio-Clone/', // Add this line to specify the base path for GitHub Pages
 });
